6a88da5838f8c8cd0ce67fdce27d54797dd6f0a3
Karanveer-singh671 <singhkaranveer671@gmail.com>
2017-09-19 01:48:45 +0000

    add loop exercises

---
+++ chorus-for.js
@@ -0,0 +1,6 @@
+var chorus = "Let's dance!";
+for (var repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+console.log("Until the sun comes up!");
+// put in repeat var for the for loop and use the variable declared for the body
